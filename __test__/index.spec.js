c758dd05aa7749214406b8caf66d55eefd1880e1
Cesar Boaventura <cesar.boaventura61@gmail.com>
2021-03-07 23:49:25 -0300

    Squashed commit of the following:
    commit f301e542a52c424560295c045705c3e8f9388040
    Author: Cesar Boaventura <cesar.boaventura61@gmail.com>
    Date:   Sun Mar 7 23:47:24 2021 -0300
    
        chore: update eslint and add prettier

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,56 +1,56 @@
-import puppeteer from 'puppeteer'
-import faker from 'faker'
+import puppeteer from 'puppeteer';
+import faker from 'faker';
 
-const PAGE_URL = 'http://localhost:1234/'
+const PAGE_URL = 'http://localhost:1234/';
 
-let page
-let browser
+let page;
+let browser;
 
-const width = 600
-const height = 900
+const width = 600;
+const height = 900;
 
 const search = {
-  term: faker.random.word()
-}
+	term: faker.random.word(),
+};
 
 beforeAll(async () => {
-  const debugMode = process.env.NODE_ENV === 'debug'
-  browser = await puppeteer.launch({
-    headless: !debugMode,
-    slowMo: debugMode ? 50 : 0,
-    args: [`--window-size=${width},${height}`, '--no-sandbox', '--disable-setuid-sandbox']
-  })
-  page = await browser.newPage()
-  page.setViewport({ width, height })
-})
+	const debugMode = process.env.NODE_ENV === 'debug';
+	browser = await puppeteer.launch({
+		headless: !debugMode,
+		slowMo: debugMode ? 50 : 0,
+		args: [
+			`--window-size=${width},${height}`,
+			'--no-sandbox',
+			'--disable-setuid-sandbox',
+		],
+	});
+	page = await browser.newPage();
+	page.setViewport({ width, height });
+});
 
 afterAll(() => {
-  browser.close()
-})
+	browser.close();
+});
 
-const waitForLoad = page =>
-  new Promise(resolve => {
-    page.on('request', req => {
-      waitForLoad(page)
-    })
-    page.on('requestfinished', req => {
-      setTimeout(() => resolve('idle'), 0)
-    })
-  })
+const waitForLoad = (page) =>
+	new Promise((resolve) => {
+		page.on('request', (req) => {
+			waitForLoad(page);
+		});
+		page.on('requestfinished', (req) => {
+			setTimeout(() => resolve('idle'), 0);
+		});
+	});
 
 describe('Search Gif', () => {
-  it(
-    'Should search and display a gif',
-    async () => {
-      await page.goto(PAGE_URL)
-      await page.waitForSelector('#searchTerm')
-      await page.click('#searchTerm')
-      await page.type('#searchTerm', search.term)
-      await page.click('#searchGif')
-      await waitForLoad(page)
-      const gifImage = await page.$eval('#gifImage', el => el.src)
-      expect(gifImage).not.toEqual('')
-    },
-    16000
-  )
-})
+	it('Should search and display a gif', async () => {
+		await page.goto(PAGE_URL);
+		await page.waitForSelector('#searchTerm');
+		await page.click('#searchTerm');
+		await page.type('#searchTerm', search.term);
+		await page.click('#searchGif');
+		await waitForLoad(page);
+		const gifImage = await page.$eval('#gifImage', (el) => el.src);
+		expect(gifImage).not.toEqual('');
+	}, 16000);
+});
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,75 +1,75 @@
 const state = {
-  term: ''
-}
+	term: '',
+};
 
-const API_KEY = '0qyagM2WTPkO5DQgT3mcFPZfiwzxmJr5'
+const API_KEY = '0qyagM2WTPkO5DQgT3mcFPZfiwzxmJr5';
 
-const searchButton = document.getElementById('searchGif')
-const searchInput = document.getElementById('searchTerm')
-const gifImageTag = document.getElementById('gifImage')
-const loading = document.getElementById('loadingContainer')
+const searchButton = document.getElementById('searchGif');
+const searchInput = document.getElementById('searchTerm');
+const gifImageTag = document.getElementById('gifImage');
+const loading = document.getElementById('loadingContainer');
 
-const searchGif = async term => {
-  const res = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${term}&limit=25&offset=0`
-  )
+const searchGif = async (term) => {
+	const res = await fetch(
+		`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${term}&limit=25&offset=0`,
+	);
 
-  return res.json()
-}
+	return res.json();
+};
 
-const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const changeImgSrc = (imgTag, src) =>
-  new Promise((resolve, reject) => {
-    imgTag.src = src
-    imgTag.onload = () => resolve()
-  })
+	new Promise((resolve, reject) => {
+		imgTag.src = src;
+		imgTag.onload = () => resolve();
+	});
 
 const handleElementVisibility = (element, state) =>
-  (element.style.display = state)
+	(element.style.display = state);
 
-const changeStateItem = (scope, key, val) => (scope[key] = val)
+const changeStateItem = (scope, key, val) => (scope[key] = val);
 
-const changeStateTerm = e => changeStateItem(state, 'term', e.target.value)
+const changeStateTerm = (e) => changeStateItem(state, 'term', e.target.value);
 
-const changeGif = async term => {
-  const gifObj = await searchGif(term)
-  const randomIndex = random(0, gifObj.data.length - 1)
-  const gifUrl =
-    gifObj.data.length === 0
-      ? 'https://cdn.dribbble.com/users/547471/screenshots/3063720/not_found.gif'
-      : await gifObj.data[randomIndex].images.original.url
+const changeGif = async (term) => {
+	const gifObj = await searchGif(term);
+	const randomIndex = random(0, gifObj.data.length - 1);
+	const gifUrl =
+		gifObj.data.length === 0
+			? 'https://cdn.dribbble.com/users/547471/screenshots/3063720/not_found.gif'
+			: await gifObj.data[randomIndex].images.original.url;
 
-  await changeImgSrc(gifImageTag, gifUrl)
-}
+	await changeImgSrc(gifImageTag, gifUrl);
+};
 
 const resetSearch = (inputTag, scope) => {
-  inputTag.value = ''
-  inputTag.blur()
-  changeStateItem(scope, 'term', '')
-}
+	inputTag.value = '';
+	inputTag.blur();
+	changeStateItem(scope, 'term', '');
+};
 
-const handleFormSubmit = async e => {
-  e.preventDefault()
+const handleFormSubmit = async (e) => {
+	e.preventDefault();
 
-  const term = state.term
+	const term = state.term;
 
-  if (term.trim() === '') {
-    return
-  }
+	if (term.trim() === '') {
+		return;
+	}
 
-  try {
-    handleElementVisibility(loading, 'flex')
-    resetSearch(searchInput, state)
-    await changeGif(term)
-  } catch (error) {
-    throw new Error(error)
-  } finally {
-    handleElementVisibility(loading, 'none')
-    searchInput.focus()
-  }
-}
+	try {
+		handleElementVisibility(loading, 'flex');
+		resetSearch(searchInput, state);
+		await changeGif(term);
+	} catch (error) {
+		throw new Error(error);
+	} finally {
+		handleElementVisibility(loading, 'none');
+		searchInput.focus();
+	}
+};
 
-searchButton.addEventListener('click', handleFormSubmit)
+searchButton.addEventListener('click', handleFormSubmit);
 
-searchInput.addEventListener('input', changeStateTerm)
+searchInput.addEventListener('input', changeStateTerm);
